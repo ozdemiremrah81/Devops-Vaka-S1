1e5ad3f7d4716cfa649bdc040a85693b8e35778a
Emrah Ozdemir <43759247+ozdemiremrah81@users.noreply.github.com>
2024-11-29 13:04:54 +0300

    helm kurulumu değiştirildi

--- client/src/components/create.js
+++ client/src/components/create.js
@@ -23,7 +23,7 @@ export default function Create() {
    // When a post request is sent to the create url, we'll add a new record to the database.
    const newPerson = { ...form };
 
-   await fetch("http://16.171.111.166:5050/record", {
+   await fetch("http://16.16.163.150:5050/record", {
      method: "POST",
      headers: {
        "Content-Type": "application/json",

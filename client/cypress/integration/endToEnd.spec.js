b473c56adbc044c9cc39f1b4b1402812006b8e23
Emrah Ozdemir <43759247+ozdemiremrah81@users.noreply.github.com>
2024-11-30 12:48:58 +0300

    test screenshot ları eklendi

--- client/cypress/integration/endToEnd.spec.js
+++ client/cypress/integration/endToEnd.spec.js
@@ -7,7 +7,7 @@ describe("Web site availability", () => {
     cy.contains("Create Record").should("exist");
   });
   it("Test Adding Employee listings", () => {
-    cy.visit("http://16.16.163.150:32000/records");
+    cy.visit("http://16.16.163.150:32000/create");
     cy.get("#name").type("Employee1");
     cy.get("#position").type("Position1");
     cy.get("#positionIntern").click({ force: true });

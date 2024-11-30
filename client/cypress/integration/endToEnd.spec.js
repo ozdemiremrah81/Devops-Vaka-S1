81613278095a37a0ef1b43fa6a8606f72ab13fd7
Emrah Ozdemir <43759247+ozdemiremrah81@users.noreply.github.com>
2024-11-30 12:34:08 +0300

    test screenshot ları eklendi

--- client/cypress/integration/endToEnd.spec.js
+++ client/cypress/integration/endToEnd.spec.js
@@ -7,12 +7,12 @@ describe("Web site availability", () => {
     cy.contains("Create Record").should("exist");
   });
   it("Test Adding Employee listings", () => {
-    cy.visit("http://16.16.163.150:32000/create");
+    cy.visit("http://16.16.163.150:32000/records");
     cy.get("#name").type("Employee1");
     cy.get("#position").type("Position1");
     cy.get("#positionIntern").click({ force: true });
     cy.contains("Create person").click({ force: true });
-    cy.visit("http://16.16.163.150:32000");
+    cy.visit("http://16.16.163.150:32000/records");
     cy.contains("Employee1").should("exist");
   });
 });

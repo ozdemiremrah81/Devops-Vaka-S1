01f2aac6a0ca27aa4f838836c8b5963bd02083cf
Emrah Ozdemir <43759247+ozdemiremrah81@users.noreply.github.com>
2024-11-30 10:00:34 +0300

    test test ediliyor

--- client/cypress/integration/endToEnd.spec.js
+++ client/cypress/integration/endToEnd.spec.js
@@ -1,31 +1,18 @@
 describe("Web site availability", () => {
-  const baseUrl = "http://16.16.163.150:32000";
-
   after(() => {
-    // Cleanup action: Deletes the created record
     cy.contains("Delete").click({ force: true });
   });
-
-  it("Sanity check for the web site", () => {
-    // Visits the homepage and checks for the existence of "Create Record"
-    cy.visit(baseUrl);
+  it("Sanity listings web site", () => {
+    cy.visit("http://16.16.163.150:3000");
     cy.contains("Create Record").should("exist");
   });
-
-  it("Test adding an employee listing", () => {
-    // Navigate to the create record page
-    cy.visit(`${baseUrl}/create`);
-
-    // Fill out the form
+  it("Test Adding Employee listings", () => {
+    cy.visit("http://16.16.163.150:3000/create");
     cy.get("#name").type("Employee1");
     cy.get("#position").type("Position1");
     cy.get("#positionIntern").click({ force: true });
-
-    // Submit the form
     cy.contains("Create person").click({ force: true });
-
-    // Verify the new employee record is displayed on the homepage
-    cy.visit(baseUrl);
+    cy.visit("http://16.16.163.150:3000");
     cy.contains("Employee1").should("exist");
   });
 });

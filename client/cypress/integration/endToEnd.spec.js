18c37c059cc3766875a5aa7571fb9203d4d5936b
Emrah Ozdemir <43759247+ozdemiremrah81@users.noreply.github.com>
2024-11-30 10:03:36 +0300

    test test ediliyor

--- client/cypress/integration/endToEnd.spec.js
+++ client/cypress/integration/endToEnd.spec.js
@@ -3,16 +3,16 @@ describe("Web site availability", () => {
     cy.contains("Delete").click({ force: true });
   });
   it("Sanity listings web site", () => {
-    cy.visit("http://16.16.163.150:3000");
+    cy.visit("http://16.16.163.150:32000");
     cy.contains("Create Record").should("exist");
   });
   it("Test Adding Employee listings", () => {
-    cy.visit("http://16.16.163.150:3000/create");
+    cy.visit("http://16.16.163.150:32000/create");
     cy.get("#name").type("Employee1");
     cy.get("#position").type("Position1");
     cy.get("#positionIntern").click({ force: true });
     cy.contains("Create person").click({ force: true });
-    cy.visit("http://16.16.163.150:3000");
+    cy.visit("http://16.16.163.150:32000");
     cy.contains("Employee1").should("exist");
   });
 });

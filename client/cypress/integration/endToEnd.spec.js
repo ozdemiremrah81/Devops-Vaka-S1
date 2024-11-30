28c8e8ba646130f0e324f0c4ddc20450d352a18b
Emrah Ozdemir <43759247+ozdemiremrah81@users.noreply.github.com>
2024-11-30 08:29:05 +0300

    test test ediliyor

--- client/cypress/integration/endToEnd.spec.js
+++ client/cypress/integration/endToEnd.spec.js
@@ -1,13 +1,3 @@
-describe('Validate API response directly', () => {
-  it('Check API response', () => {
-    cy.request('GET', '/records').then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body.data).to.have.length(2);
-      expect(response.body.data[0]).to.have.property('name', 'John Doe');
-    });
-  });
-});
-
 describe("Web site availability", () => {
   after(() => {
     cy.contains("Delete").click({ force: true });

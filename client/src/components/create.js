a2f32be6c5092fe44e5dc03eef7414dc035f8a67
Emrah Ozdemir <43759247+ozdemiremrah81@users.noreply.github.com>
2024-12-02 11:36:32 +0300

    sertifika eklendi

--- client/src/components/create.js
+++ client/src/components/create.js
@@ -23,7 +23,7 @@ export default function Create() {
    // When a post request is sent to the create url, we'll add a new record to the database.
    const newPerson = { ...form };
 
-   await fetch("http://16.16.163.150:32050/record", {
+   await fetch("https://vaka1.m365.net.tr:32050/record", {
      method: "POST",
      headers: {
        "Content-Type": "application/json",

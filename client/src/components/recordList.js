37d484bacaae54de903cbba01529bb7f04002b89
Emrah Ozdemir <43759247+ozdemiremrah81@users.noreply.github.com>
2024-12-02 12:16:37 +0300

    ssl kaldırıldı

--- client/src/components/recordList.js
+++ client/src/components/recordList.js
@@ -25,7 +25,7 @@ export default function RecordList() {
  // This method fetches the records from the database.
  useEffect(() => {
    async function getRecords() {
-     const response = await fetch(`https://vaka1.m365.net.tr:32050/record/`);
+     const response = await fetch(`https://16.16.163.150:32050/record/`);
 
      if (!response.ok) {
        const message = `An error occurred: ${response.statusText}`;
@@ -44,7 +44,7 @@ export default function RecordList() {
 
  // This method will delete a record
  async function deleteRecord(id) {
-   await fetch(`https://vaka1.m365.net.tr:32050/record/${id}`, {
+   await fetch(`https://16.16.163.150:32050/record/${id}`, {
      method: "DELETE"
    });
 
